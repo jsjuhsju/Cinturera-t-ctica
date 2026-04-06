8bcd44fe0f018d0a7b252f1cde9b1722eb3cb921
jsjuhsju <jsjuhsju240@gmail.com>
2026-04-06 12:55:46 -0500

    Crear fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Tactical Belt - Script de Cinturera'
+author 'Tu Nombre'
+version '1.0.0'
+
+-- Le decimos que use las funciones de ESX
+shared_script '@es_extended/imports.lua'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
